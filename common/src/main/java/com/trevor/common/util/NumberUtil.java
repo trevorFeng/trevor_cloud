df789634d341d0a31b8e87846dbdfb4743c72cdf
trevorl <trevorl@synnex.com>
2019-08-19 19:53:22 +0800

    init

---
+++ common/src/main/java/com/trevor/common/util/NumberUtil.java
@@ -0,0 +1,12 @@
+package com.trevor.common.util;
+
+public class NumberUtil {
+
+    public static Integer stringFormatInteger(String numebr){
+        return Integer.valueOf(numebr);
+    }
+
+    public static String formatString(Object obj) {
+        return String.valueOf(obj);
+    }
+}
