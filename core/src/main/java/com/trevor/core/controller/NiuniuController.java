8ef8d71f0be3523b0f4050c154f4aaf3fd6bf6ea
trevorl <trevorl@synnex.com>
2019-08-22 20:08:34 +0800

    init

--- core/src/main/java/com/trevor/core/controller/NiuniuController.java
+++ core/src/main/java/com/trevor/core/controller/NiuniuController.java
@@ -17,14 +17,35 @@ public class NiuniuController {
     @Resource
     private NiuniuService niuniuService;
 
+    /**
+     * 两个人开始游戏，注册发4张牌事件
+     * @param roomId
+     * @return
+     */
     @RequestMapping(value = "/api/niuniu/playTwo/{roomId}", method = RequestMethod.GET)
     public JsonEntity<Object> playTwo(@PathVariable String roomId){
 
         return ResponseHelper.createInstance(null , MessageCodeEnum.HANDLER_SUCCESS);
     }
 
+    /**
+     * 超过2个人开始游戏，注册准备倒计时事件
+     * @param roomId
+     * @return
+     */
     @RequestMapping(value = "/api/niuniu/playOverTwo/{roomId}", method = RequestMethod.GET)
     public JsonEntity<Object> playOverTwo(@PathVariable String roomId){
+
+        return ResponseHelper.createInstance(null , MessageCodeEnum.HANDLER_SUCCESS);
+    }
+
+    /**
+     * 若在准备倒计时中，所有人都已经准备好，删除准备倒计时事件并且注册发4张牌事件
+     * @param roomId
+     * @return
+     */
+    @RequestMapping(value = "/api/niuniu/allReady/{roomId}", method = RequestMethod.GET)
+    public JsonEntity<Object> allReady(@PathVariable String roomId){
 
         return ResponseHelper.createInstance(null , MessageCodeEnum.HANDLER_SUCCESS);
     }
--- core/src/main/java/com/trevor/core/listener/ListenerConfig.java
+++ core/src/main/java/com/trevor/core/listener/ListenerConfig.java
@@ -1,6 +1,9 @@
 package com.trevor.core.listener;
 
+import com.trevor.common.bo.RedisConstant;
+import com.trevor.common.bo.SocketResult;
 import com.trevor.common.service.RedisService;
+import com.trevor.common.util.JsonUtil;
 import com.trevor.core.schedule.ScheduleDispatch;
 
 import javax.annotation.Resource;
@@ -14,5 +17,14 @@ public abstract class ListenerConfig implements TaskListener{
     @Resource
     public void setRedisService(RedisService redisService){
         ListenerConfig.redisService = redisService;
+    }
+
+    /**
+     * 给玩家发消息
+     * @param socketResult
+     * @param playerId
+     */
+    protected void sendMessage(SocketResult socketResult , String playerId){
+        redisService.listRightPush(RedisConstant.MESSAGES_QUEUE + playerId , JsonUtil.toJsonString(socketResult));
     }
 }
